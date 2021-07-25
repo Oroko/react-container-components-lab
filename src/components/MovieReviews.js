86fe9deeac4a3bd567153f6f411244c729785a8d
Oroko <aoroko09@gmail.com>
2021-07-25 17:23:33 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,48 @@
 // Code MovieReviews Here
+import React from 'react'
+
+function MovieReviews({reviews}) {
+  return (
+    <div className="review-list">
+      {reviews.map((review) => (
+        <div key={review.headline} className="review">
+          
+          <header>
+            
+            <a className="review-link" href={review.link.url}>
+              {review.headline}
+              
+            </a>
+            <br />
+            <span className="author">{review.byline}</span>
+            
+          </header>
+          <blockquote>{review.summary_short}</blockquote>
+          
+        </div>
+      ))}
+    </div>
+  );
+}
+
+// const Review = ({ headline, byline, link, summary_short }) => {
+//   return (
+//     <div key={headline} className="review">
+//       <header>
+//         <a className="review-link" href={link.url}>
+//           {headline}
+//         </a>
+//         <br />
+//         <span className="author">{byline}</span>
+//       </header>
+//       <blockquote>{summary_short}</blockquote>
+//     </div>
+//   );
+// };
+
+// const MovieReviews = ({ reviews }) => (
+//   <div className="review-list">{reviews.map(Review)}</div>
+// );
+
+export default MovieReviews
+
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,53 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = "khLWBu8D9NRoE0thrsv2yJM3Co1e6iEB";
+const URL =
+  "https://api.nytimes.com/svc/movies/v2/reviews/search.json?" +
+  `api-key=${NYT_API_KEY}&query=`;
 
 // Code SearchableMovieReviewsContainer Here
+
+
+
+export class SearchableMovieReviewsContainer extends Component {
+  state={
+    reviews: [],
+    searchTerm: ''
+  }
+  handleSearchInputChange = (e) => {
+    this.setState({searchTerm: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    fetch(URL.concat(this.state.searchTerm))
+    .then(res => res.json())
+    .then(data => this.setState({reviews: data.results}))
+  }
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="search-input">Search Movie Reviews</label>
+          <input
+            id="search-input"
+            type="text"
+            style={{ width: 300 }}
+            value={this.state.searchTerm}
+            onChange={this.handleSearchInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        {typeof this.state.reviews === "object" &&
+          this.state.reviews.length > 0 && <h2>Movie Review By Search:</h2>}
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}//{" "}
+//{" "}
+//{" "}
+
+export default SearchableMovieReviewsContainer
+
