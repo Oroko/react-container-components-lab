44e7201f0b210c6fca394f5de627806a9818f405
Oroko <aoroko09@gmail.com>
2021-07-25 17:23:29 -0400

    commit tree d49bfa99422c98de9613aa5d46f4489fe84f727e

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,38 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = "khLWBu8D9NRoE0thrsv2yJM3Co1e6iEB";
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component{
+  constructor(){
+    super()
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentDidMount(){
+    this.getMovies()
+  }
+
+  getMovies = () => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((reviewData) => this.setState({ reviews: reviewData.results }))
+      .catch((error) => console.log(error));
+  }
+
+  render(){
+    return(
+      <div className='latest-movie-reviews'>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
+
+export default LatestMovieReviewsContainer
